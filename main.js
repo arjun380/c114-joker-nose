2cfa29a67bf26202884bf6c04f1fd27808e66b80
arjun380 <82092827+arjun380@users.noreply.github.com>
2021-10-08 15:46:59 +0530

    100 % stuff  done
    Postnet, loadimage, save function

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-function preload() {
+maskX=0;
+maskY=0;
 
+function preload() {
+clown_nose =
+loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup(){
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded() {
+    console.log('PoseNet Is Initilized');
+}
+
+function gotPoses(results) {
+if(results.length > 0)
+{
+    console.log(results);
+    maskX = results[0].pose.nose.x-15;
+    maskY = results[0].pose.nose.y-15;
+}
+}
 function draw() {
-
+image(video,0, 0, 300, 300);
+image(clown_nose, maskX, maskY, 30, 30);
 }
 
-function take_snapshort(){
+function take_snaphot(){
     save('myFilterImage.png');
 }
 
